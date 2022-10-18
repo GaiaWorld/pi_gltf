ff011abf5be2b7334c2e0a581ed40a92a7784959
zhuy <2832226065@qq.com>
2022-10-18 16:22:42 +0800

    feat: 自定义 rust库 gltf

---
+++ gltf-json/src/extras.rs
@@ -0,0 +1,48 @@
+use gltf_derive::Validate as MValidate;
+use serde_derive::{Deserialize, Serialize};
+use std::fmt;
+
+#[cfg(feature = "extras")]
+pub use serde_json::value::Value;
+#[cfg(feature = "extras")]
+use crate::{
+    validation::{Error, Validate},
+    Path, Root,
+};
+
+/// Data type of the `extras` attribute on all glTF objects.
+#[cfg(feature = "extras")]
+pub type Extras = Option<::std::boxed::Box<Value>>;
+
+#[cfg(feature = "extras")]
+impl Validate for Extras {
+    fn validate<P, R>(&self, root: &Root, path: P, report: &mut R)
+    where
+        P: Fn() -> Path,
+        R: FnMut(&dyn Fn() -> Path, Error),
+    {
+    }
+}
+
+/// Data type of the `extras` attribute on all glTF objects.
+#[cfg(not(feature = "extras"))]
+pub type Extras = Void;
+
+/// Type representing no user-defined data.
+#[derive(Clone, Default, Serialize, Deserialize, MValidate)]
+pub struct Void {
+    #[serde(default, skip_serializing)]
+    _allow_unknown_fields: (),
+}
+
+impl fmt::Debug for Void {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{}}")
+    }
+}
+
+impl fmt::Display for Void {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{}}")
+    }
+}
